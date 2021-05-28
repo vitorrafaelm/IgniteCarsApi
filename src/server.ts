5366dd2475d39a38b971e683eed82c0ff1bed5c9
vitorrafaelm <vitor.rafael1518@gmail.com>
2021-05-28 17:15:06 -0300

    Learning how to document an api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUI from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
