563266d03937e64efad399ffe2d74ed319d546e7
vitorrafaelm <vitor.rafael1518@gmail.com>
2021-06-01 16:56:08 -0300

    Setting up the database connection

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -5,6 +6,8 @@ import swaggerUI from 'swagger-ui-express';
 
 import { router } from './routes';
 import swaggerFile from './swagger.json';
+
+import './database';
 
 const app = express();
 
